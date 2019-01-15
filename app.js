e194b8db729996161e320e2df7a80e168219a183
SergeyRykov <rykcer@mail.ru>
2019-01-15 16:14:24 +0300

    Выравнивание кода JS/Pretiffy
    Дополнительный текст, нужно было выравнять
    код, так как он сливался.
    
    [ticket: X]

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-(function () {
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('NameCalculator', [])
+    angular.module('NameCalculator', [])
 
-.controller('NameCalculatorController', function ($scope) {
-  $scope.name = "";
-  $scope.totalValue = 0;
+        .controller('NameCalculatorController', function($scope) {
+            $scope.name = "";
+            $scope.totalValue = 0;
 
-  $scope.displayNumeric = function () {
-    var totalNameValue = calculateNumericForString($scope.name);
-    $scope.totalValue = totalNameValue;
-  };
+            $scope.displayNumeric = function() {
+                var totalNameValue = calculateNumericForString($scope.name);
+                $scope.totalValue = totalNameValue;
+            };
 
 
-  function calculateNumericForString(string) {
-    var totalStringValue = 0;
-    for (var i = 0; i < string.length; i++) {
-      totalStringValue += string.charCodeAt(i);
-    }
+            function calculateNumericForString(string) {
+                var totalStringValue = 0;
+                for (var i = 0; i < string.length; i++) {
+                    totalStringValue += string.charCodeAt(i);
+                }
 
-    return totalStringValue;
-  }
+                return totalStringValue;
+            }
 
-}); 
+        });
 
 })();
